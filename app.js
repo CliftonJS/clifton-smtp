d98923fe1c699eeaeeabd827dfaa90567a77c0b8
Oliver Barnwell <blastdoors666@gmail.com>
2015-04-02 19:40:27 +0100

    Change

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 //TEMPORARY
 var smtp = require('./lib/clifton-smtp');
 var server = smtp.createServer({});
+server.listen(25, {}, function(err) { 	});
+server.on("mail-received", function(d) {
+	console.log("mail received event fired!");
+	console.log(d);
+});
